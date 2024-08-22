17332931486fe69731585cc67257f70b49fd615a
Wei Kiat <weikiatloh2007@gmail.com>
2024-08-22 12:43:54 +0800

    changed app.py and fixed index.html

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
 
 from flask import Flask, render_template, url_for
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/' , methods=['POST', 'GET'])
 def index():
     return render_template('index.html')
 
-@app.route('/project')
+@app.route('/project', methods=['POST', 'GET'])
 def project():
     return render_template('projects.html')
 
-@app.route('/ctf')
+@app.route('/ctf', methods=['POST', 'GET'])
 def ctf():
     return render_template('ctf.html')
 
-@app.route('/flag')
+@app.route('/flag',methods=['POST', 'GET'])
 def flag():
     path = "flag.txt"
     with open(path, 'r') as file:
@@ -23,5 +24,6 @@ def flag():
     return render_template('flag.html',content=content)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+   port = int(os.environ.get('PORT', 88))
+   app.run(debug=True, host='0.0.0.0', port=port)
     
